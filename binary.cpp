bf6c454f3c3bb1af3580bdf6ab8490f22d6dc772
Sam Barratt <milktheelephant@gmail.com>
2014-05-20 17:12:12 +0100

    Added help message and ability to display any number in its binary form.

--- binary.cpp
+++ binary.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <bitset>
+int nu; //the number specified to convert to binary.
 
 int main (int argc, char* argv[])
 {
 
-    std::cout << "| 128 |  64 |  32 | 16 | 8 | 4 | 2 | 1 |\n";
+    if (argc > 1)
+    {
+        if (strncmp(argv[1], "-h", 2) == 0)
+            {
+                std::cout << "binary [options] [number]\n-h    Displays this message.\n-d    Displays numbering scheme.\n-t    Takes number and displays it in binary\n"; //display help message.
+            }   
+
+        else if (strncmp(argv[1], "-d", 2)  == 0)
+            { 
+                std::cout << "| 128 |  64 |  32 | 16 | 8 | 4 | 2 | 1 |\n"; //display binary numbering scheme.
+            }
+        
+        else if ( strncmp (argv[1], "-t", 2) == 0 )
+            {
+                nu = atoi(argv[2]); //convert input to integer
+                std::bitset<>x(nu); //display integer's binary form.
+                std::cout << x << "\n";
+            }    
+        else 
+            {
+                std::cout << "binary [options] [number]\n try using -h\n";
+            }
+    }
+       
+    else 
+    {
+       std::cout << "binary [options] [number]\n-h    Displays this message.\n-d    Displays numbering scheme.\n-t    Takes number and displays it in binary\n";
+    }   
+   
     return 0;
 
 }
